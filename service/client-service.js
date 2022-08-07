108ad78262d246e23dde08092c157a7fc4090cd3
ayelenleclerc <ayelenleclerc@gmail.com>
2022-08-07 13:01:55 -0300

    registro con db terminado

--- service/client-service.js
+++ service/client-service.js
@@ -1,70 +1,53 @@
-//backticks
-const crearNuevaLinea = (nombre, email) => {
-  const linea = document.createElement("tr");
-  const contenido = `
-    <td class="td" data-td>
-      ${nombre}
-    </td>
-    <td>${email}</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-          >
-            Editar
-          </a>
-        </li>
-        <li>
-          <button class="simple-button simple-button--delete" type="button">
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  `;
-  linea.innerHTML = contenido;
-  return linea;
-};
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((response) => response.json());
 
-const table = document.querySelector("[data-table]");
-
-//Abrir http (método,url)
-
-// CRUD   - Métodos HTTP
-// Create - POST
-// Read   - GET
-// Update - PUT/PATCH
-// Delete - DELETE
-
-const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.open("GET", "http://localhost:3000/perfiles");
-
-    http.send();
-
-    http.onload = () => {
-      const response = JSON.parse(http.response);
-      if (http.status >= 400) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
-    };
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nombre,
+      email,
+      id: uuid.v4(),
+    }),
   });
-  return promise;
 };
 
-listaClientes()
-  .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-      table.appendChild(nuevaLinea);
-    });
-  })
-  .catch((error) => alert("Ocurrió un error"));
+const eliminarCLiente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE",
+  });
+};
 
-// console.log(data);
-//
+const detalleCliente = async (id) => {
+  const response = await fetch(`http://localhost:3000/perfil/${id}`);
+  return await response.json();
+};
+
+const actualizarCLiente = async (nombre, email, id) => {
+  try {
+    const response = await fetch(`http://localhost:3000/perfil/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre,
+        email,
+        id,
+      }),
+    });
+    return await response.json();
+  } catch (err) {
+    return console.log(err);
+  }
+};
+export const clientServices = {
+  listaClientes,
+  crearCliente,
+  eliminarCLiente,
+  detalleCliente,
+  actualizarCLiente,
+};
